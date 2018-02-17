3aa46b3210541f1cdc83abb1bf1da3bef84d12e1
Jason R Tibbetts <jason@tibbetts.net>
2018-02-16 23:09:18 -0500

    Initial Commit

---
+++ SwiftGenius/SwiftGenius.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGenius.h
+//  SwiftGenius
+//
+//  Created by Jason R Tibbetts on 2/16/18.
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGenius.
+FOUNDATION_EXPORT double SwiftGeniusVersionNumber;
+
+//! Project version string for SwiftGenius.
+FOUNDATION_EXPORT const unsigned char SwiftGeniusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGenius/PublicHeader.h>
+
+
