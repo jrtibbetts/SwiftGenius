c7094ef8523ca2e2dc376dc0dfc92a1373d14889
Jason R Tibbetts <jason@tibbetts.net>
2019-02-22 21:39:01 -0500

    No ticket: Streamlined the MockGeniusTests.

--- SwiftGeniusTests/MockGeniusTests.swift
+++ SwiftGeniusTests/MockGeniusTests.swift
@@ -7,131 +7,116 @@ import XCTest
 class MockGeniusTests: ClientTestBase {
 
     func testAccountReturnsValidJson() {
-        assertValidJson(description: "user response") { (genius) -> Promise<GeniusAccount.Response> in
-            genius.account()
-        }
+        assertValidJson(description: "user response",
+                        promise: MockGenius().account())
     }
 
     func testAccountReturnsInErrorMode() {
-        assertInvalidJson(description: "user response") { (genius) -> Promise<GeniusAccount.Response> in
-            genius.account()
-        }
+        assertInvalidJson(description: "user response",
+                          promise: MockGenius(useErrorMode: true).account())
     }
 
     func testAnnotationReturnsValidJson() {
-        _ = assertValidJson(description: "annotation response") { (genius) -> Promise<GeniusAnnotation.Response> in
-            genius.annotation(id: 99)
-            }.done { (annotationResponse) in
-                GeniusAnnotationTests.assert(annotationResponse)
+        _ = assertValidJson(description: "annotation response",
+                            promise: MockGenius().annotation(id: 99)).done { (annotationResponse) in
+            GeniusAnnotationTests.assert(annotationResponse)
         }
     }
 
     func testAnnotationInErrorMode() {
-        assertInvalidJson(description: "annotation response") { (genius) -> Promise<GeniusAnnotation.Response> in
-            genius.annotation(id: 99)
-        }
+        assertInvalidJson(description: "annotation response",
+                          promise: MockGenius(useErrorMode: true).annotation(id: 99))
     }
 
     func testArtistReturnsValidJson() {
-        _ = assertValidJson(description: "artist response") { (genius) -> Promise<GeniusArtist.Response> in
-            genius.artist(id: 99)
-            }.done { (artistResponse) in
-                GeniusArtistTests.assert(artistResponse)
+        _ = assertValidJson(description: "artist response",
+                            promise: MockGenius().artist(id: 99)).done { (artistResponse) in
+            GeniusArtistTests.assert(artistResponse)
         }
     }
 
     func testArtistInErrorMode() {
-        assertInvalidJson(description: "artist response") { (genius) -> Promise<GeniusArtist.Response> in
-            genius.artist(id: 99)
-        }
+        assertInvalidJson(description: "artist response",
+                          promise: MockGenius(useErrorMode: true).artist(id: 99))
     }
 
     func testSongsByArtistReturnsValidJson() {
-        _ = assertValidJson(description: "artist songs response") { (genius) -> Promise<GeniusArtistSongs.Response> in
-            genius.songs(byArtistId : 99)
-            }.done { (songsResponse) in
-                GeniusArtistSongsTests.assert(songsResponse)
+        _ = assertValidJson(description: "artist songs response",
+                            promise: MockGenius().songs(byArtistId : 99)).done { (songsResponse) in
+            GeniusArtistSongsTests.assert(songsResponse)
         }
     }
 
     func testSongsByArtistInErrorMode() {
-        assertInvalidJson(description: "artist songs response") { (genius) -> Promise<GeniusArtistSongs.Response> in
-            genius.songs(byArtistId: 99)
-        }
+        assertInvalidJson(description: "artist songs response",
+                          promise: MockGenius(useErrorMode: true).songs(byArtistId: 99))
     }
 
     func testReferentsReturnsValidJson() {
-        _ = assertValidJson(description: "referents response") { (genius) -> Promise<GeniusReferent.Response> in
-            genius.referents(forSongId: 99)
-            }.done { (referentsResponse) in
-                GeniusReferentTests.assert(referentsResponse)
+        _ = assertValidJson(description: "referents response",
+                            promise: MockGenius().referents(forSongId: 99)).done { (referentsResponse) in
+            GeniusReferentTests.assert(referentsResponse)
         }
     }
 
     func testReferentsInErrorMode() {
-        assertInvalidJson(description: "referents response") { (genius) -> Promise<GeniusReferent.Response> in
-            genius.referents(forSongId: 99)
-        }
+        assertInvalidJson(description: "referents response",
+                          promise: MockGenius(useErrorMode: true).referents(forSongId: 99))
     }
 
     func testSearchReturnsValidJson() {
-        assertValidJson(description: "search results") { (genius) -> Promise<GeniusSearch.Response> in
-            genius.search(terms: "foo")
-        }
+        assertValidJson(description: "search results",
+                        promise: MockGenius().search(terms: "foo"))
     }
 
     func testSearchReturnsInErrorMode() {
-        assertInvalidJson(description: "search results") { (genius) -> Promise<GeniusSearch.Response> in
-            genius.search(terms: "foo")
-        }
+        assertInvalidJson(description: "search results",
+                          promise: MockGenius(useErrorMode: true).search(terms: "foo"))
     }
 
     func testSongReturnsValidJson() {
-        _ = assertValidJson(description: "song response") { (genius) -> Promise<GeniusSong.Response> in
-            genius.song(id: 99)
-            }.done { (songResponse) in
-                GeniusSongTests.assert(songResponse)
+        _ = assertValidJson(description: "song response",
+                            promise: MockGenius().song(id: 99)).done { (songResponse) in
+            GeniusSongTests.assert(songResponse)
         }
     }
 
     func testSongReturnsInErrorMode() {
-        assertInvalidJson(description: "song response") { (genius) -> Promise<GeniusSong.Response> in
-            genius.song(id: 99)
-        }
+        assertInvalidJson(description: "song response",
+                          promise: MockGenius(useErrorMode: true).song(id: 99))
     }
 
     func testWebPageReturnsValidJson() {
-        assertValidJson(description: "web page response") { (genius) -> Promise<GeniusWebPage.Response> in
-            genius.webPage(id: 99)
-        }
+        assertValidJson(description: "web page response",
+                        promise: MockGenius().webPage(id: 99))
     }
 
     func testWebPageReturnsInErrorMode() {
-        assertInvalidJson(description: "web page response") { (genius) -> Promise<GeniusWebPage.Response> in
-            genius.webPage(id: 99)
-        }
+        assertInvalidJson(description: "web page response",
+                          promise: MockGenius(useErrorMode: true).webPage(id: 99))
     }
 
     func testInvalidJsonRejectsPromise() {
-        assertInvalidJson(description: "broken request") { (genius) -> Promise<String> in
-            genius.brokenRequest()
-        }
+        assertInvalidJson(description: "broken request",
+                          promise: MockGenius().brokenRequest())
     }
 
     @discardableResult
     func assertValidJson<T: Codable>(description: String = "",
-                                     function: (MockGenius) -> Promise<T>) -> Promise<T> {
-        let promise = function(MockGenius())
-        assert(validPromise: promise, description: description)
+                                     file: StaticString = #file,
+                                     line: UInt = #line,
+                                     promise: Promise<T>) -> Promise<T> {
+        assert(validPromise: promise, description: description, file: file, line: line)
 
         return promise
     }
 
     @discardableResult
     func assertInvalidJson<T: Codable>(description: String = "",
-                                       function: (MockGenius) -> Promise<T>) -> Promise<T> {
-        let promise = function(MockGenius(useErrorMode: true))
-        assert(invalidPromise: promise, description: description)
+                                       file: StaticString = #file,
+                                       line: UInt = #line,
+                                       promise: Promise<T>) -> Promise<T> {
+        assert(invalidPromise: promise, description: description, file: file, line: line)
 
         return promise
     }
