db4b35044b84c146d6d210d4c59a488f7d298096
Jason R Tibbetts <jason@tibbetts.net>
2021-05-05 01:31:16 -0400

    #3: [meta] Created a sample app project to test the framework's features.

---
+++ Shared/ContentView.swift
@@ -0,0 +1,17 @@
+//  Created by Jason R Tibbetts on 5/5/21.
+
+import SwiftGenius
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftGeniusApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftGeniusAppApp.swift
+//  Shared
+//
+//  Created by Jason R Tibbetts on 5/5/21.
+//
+
+import SwiftGenius
+import SwiftUI
+
+@main
+struct SwiftGeniusApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+}
