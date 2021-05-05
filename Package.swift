d23f374f83d334ba42c4001d0075687f3284405b
Jason R Tibbetts <jason@tibbetts.net>
2021-05-05 00:12:14 -0400

    #2: [meta] Converted to a Swift package.

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let pkg = Package(
+    name: "SwiftGenius",
+
+    platforms: [
+        .iOS(.v14)
+    ],
+
+    products: [
+        .library(
+            name: "SwiftGenius",
+            targets: ["SwiftGenius"]
+        )
+    ],
+
+    dependencies: [
+        .package(url: "https://github.com/jrtibbetts/JSONClient.git",
+                 .branch("main")),
+    ],
+
+    targets: [
+        .target(name: "Sources",
+                dependencies: ["JSONClient"],
+                path: "Sources"
+        ),
+        .testTarget(name: "Tests",
+                    dependencies: ["Sources"],
+                    path: "Tests"
+                    )
+    ]
+)
+
